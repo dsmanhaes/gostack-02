f2cf9503d002166ffe928fceb727360a47e649d4
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-17 17:22:54 -0300

    fix: add default uuid value to id

--- backend/src/database/migrations/1600370474638-CreateAppointments.ts
+++ backend/src/database/migrations/1600370474638-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1600370474638
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
