102c9b02549d5e57d8d73d72a16903348af06aeb
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-18 15:32:28 -0300

    fix: changed type of fields to uuid

--- backend/src/database/migrations/1600370474638-CreateAppointments.ts
+++ backend/src/database/migrations/1600370474638-CreateAppointments.ts
@@ -9,14 +9,15 @@ export default class CreateAppointments1600370474638
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
-            type: 'varchar',
+            name: 'provider_id',
+            type: 'uuid',
+            isNullable: true,
           },
           {
             name: 'date',
