9bdb9c7e1e6c77bd7d0ddd207ecc41e80b4e1f86
Diogo Manhães <dsmanhaes@gmail.com>
2020-09-18 14:43:30 -0300

    fix: remove unnecessary isnullable false

--- backend/src/database/migrations/1600370474638-CreateAppointments.ts
+++ backend/src/database/migrations/1600370474638-CreateAppointments.ts
@@ -17,12 +17,10 @@ export default class CreateAppointments1600370474638
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
         ],
       }),
